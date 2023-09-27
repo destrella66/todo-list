bbddfc57e61f3344ff8c5b72ee94ba9b328d0eb2
Davi Estrella <daviestrella66@gmail.com>
2023-09-26 22:21:08 -0300

    Deu erro, voltando ao default

--- scr/script/scripts.js
+++ scr/script/scripts.js
@@ -1,29 +1,28 @@
+let takeNotes = document.getElementById("take-notes");
+let addNote = document.getElementById("addNote");
+let noteId = document.getElementById("noteId");
+let  noteBox = document.getElementsByClassName("noteBox");
+let receiveNotes = document.getElementById("receiveNotes")
 
-function addNote() {
-let noteId = document.getElementById("noteId").value;
 
-    if (noteId.trim() !== "") {
-    let receiveNotes = document.getElementById("receiveNotes")
+addNote.addEventListener("click", () => {
 
-
-      // Create a div element to the note
-      let noteBox = document.createElement("div");
-      noteBox.textContent = noteId;
-      noteBox.classList.toggle("noteBox")
-
-      // Create a button to remove notes
-      let deleteButton = document.createElement("button");
-      deleteButton.textContent = "X";
-      deleteButton.onclick = function () {
-        // Use "this" to match with the note that is selected
-        receiveNotes.removeChild(this.parentElement);
-      };
-
-      // Add new notes
-      noteBox.appendChild(deleteButton);
-      receiveNotes.appendChild(noteBox);
-
-      // Clear noteId input
-      noteId= "";
+    if(noteId.value == ''){
+        alert("Enter a note!");
     }
-  }
+    else{
+        receiveNotes.innerHTML +=`
+            <div class="noteBox">
+                <span>
+                    ${noteId.value}
+                </span>
+                <button class="deleteButton">Excluir</button>
+                
+            </div>
+        `;
+
+
+        noteId.value = "";
+
+    }
+});    
