67108f69beae90627231b45e9534102dd9535caa
Davi Estrella <daviestrella66@gmail.com>
2023-09-28 20:53:10 -0300

    [Atualização] Muda o código base e faz novos eventos usando banco de dados com array

--- scr/script/scripts.js
+++ scr/script/scripts.js
@@ -1,28 +1,76 @@
-let takeNotes = document.getElementById("take-notes");
 let addNote = document.getElementById("addNote");
 let noteId = document.getElementById("noteId");
-let  noteBox = document.getElementsByClassName("noteBox");
-let receiveNotes = document.getElementById("receiveNotes")
+let receiveNotes = document.getElementById("receiveNotes");
+let noteBox = document.querySelector('.noteBox');
+
+let banco = [
+    {'tarefa': 'Dormir', 'status':'checked'},
+    {'tarefa': 'Estudar', 'status':''},
+];
+
+const criarItem = (tarefa, status, indice) => {
+    const item = document.createElement('label');
+    item.classList.add('noteBox');
+    item.addEventListener('click', clickItem)
+    item.innerHTML = `  
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+        `
+    receiveNotes.appendChild(item);
+}
+
+const limparTarefas = () => {
+while(receiveNotes.firstChild){
+    receiveNotes.removeChild(receiveNotes.lastChild)
+}
+}
 
 
-addNote.addEventListener("click", () => {
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach( (item, indice) => criarItem(item.tarefa, item.status, indice))
+}
 
-    if(noteId.value == ''){
-        alert("Enter a note!");
+const inserirItem = (event) => {
+    const texto = noteId.value;
+
+    banco.push({'tarefa': texto, 'status': 'checked'})
+    noteId.value = '';
+}
+
+const enterPress = (event) => {
+    const tecla = event.key; 
+
+    if(tecla === "Enter"){
+        inserirItem();
+        atualizarTela();
     }
-    else{
-        receiveNotes.innerHTML +=`
-            <div class="noteBox">
-                <span>
-                    ${noteId.value}
-                </span>
-                <button class="deleteButton">Excluir</button>
-                
-            </div>
-        `;
+} 
+const buttonClick = () => {
+    if(noteId.value != ''){
 
-
-        noteId.value = "";
-
+        inserirItem();
+        atualizarTela();
     }
-});    
+}
+
+const removerItem = (indice) => {
+    banco.splice(indice, 1);
+    atualizarTela();
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    if(elemento.type === 'button'){
+        const indice =  elemento.dataset.indice;
+        removerItem(indice);
+    }
+}
+
+
+noteId.addEventListener("keypress", enterPress)
+addNote.addEventListener("click", buttonClick)
+document.querySelector(".noteBox").addEventListener("click", clickItem)
+
+atualizarTela();
