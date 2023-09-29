0fd77c75bf4d68af7e56b4cf51c526436267481d
Davi Estrella <daviestrella66@gmail.com>
2023-09-28 22:26:38 -0300

    [Atualização] Adciona localStorage e interações com o banco de dados

--- scr/script/scripts.js
+++ scr/script/scripts.js
@@ -3,10 +3,14 @@ let noteId = document.getElementById("noteId");
 let receiveNotes = document.getElementById("receiveNotes");
 let noteBox = document.querySelector('.noteBox');
 
-let banco = [
-    {'tarefa': 'Dormir', 'status':'checked'},
-    {'tarefa': 'Estudar', 'status':''},
-];
+// let banco = [
+//     { 'tarefa': 'Dormir', 'status': 'checked' },
+//     { 'tarefa': 'Estudar', 'status': '' },
+// ];
+
+const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem('todoList', JSON.stringify(banco))
+const banco = getBanco();
 
 const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
@@ -21,34 +25,35 @@ const criarItem = (tarefa, status, indice) => {
 }
 
 const limparTarefas = () => {
-while(receiveNotes.firstChild){
-    receiveNotes.removeChild(receiveNotes.lastChild)
-}
+    while (receiveNotes.firstChild) {
+        receiveNotes.removeChild(receiveNotes.lastChild)
+    }
 }
 
 
 const atualizarTela = () => {
     limparTarefas();
-    banco.forEach( (item, indice) => criarItem(item.tarefa, item.status, indice))
+
+    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice))
 }
 
 const inserirItem = (event) => {
     const texto = noteId.value;
-
-    banco.push({'tarefa': texto, 'status': 'checked'})
+    banco.push({ 'tarefa': texto, 'status': 'checked' })
+    setBanco(banco);
     noteId.value = '';
 }
 
 const enterPress = (event) => {
-    const tecla = event.key; 
+    const tecla = event.key;
 
-    if(tecla === "Enter"){
+    if (tecla === "Enter") {
         inserirItem();
         atualizarTela();
     }
-} 
+}
 const buttonClick = () => {
-    if(noteId.value != ''){
+    if (noteId.value != '') {
 
         inserirItem();
         atualizarTela();
@@ -57,14 +62,26 @@ const buttonClick = () => {
 
 const removerItem = (indice) => {
     banco.splice(indice, 1);
+    setBanco(banco)
     atualizarTela();
+}
+
+const atualizarItem = (indice) => {
+    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    setBanco(banco);
+
+
 }
 
 const clickItem = (evento) => {
     const elemento = evento.target;
-    if(elemento.type === 'button'){
-        const indice =  elemento.dataset.indice;
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
         removerItem(indice);
+    }
+    else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+        atualizarItem(indice);
     }
 }
 
@@ -73,4 +90,4 @@ noteId.addEventListener("keypress", enterPress)
 addNote.addEventListener("click", buttonClick)
 document.querySelector(".noteBox").addEventListener("click", clickItem)
 
-atualizarTela();
+atualizarTela(); 
